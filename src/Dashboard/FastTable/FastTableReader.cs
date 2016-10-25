Azure/azure-webjobs-sdk

Allow multiple log writers to the same storage account. Fix: https://github.com/Azure/azure-webjobs-sdk/issues/883 This is a breaking change in log format.

src/Dashboard/FastTable/FastTableReader.cs
@@ -25,35 +25,29 @@ namespace Dashboard.Data
 
         private DateTime _version = DateTime.MinValue;
 
-        private FunctionSnapshot[] _snapshots = null; // Cache of function definitions 
-
         public FastTableReader(ILogReader reader)
         {
             _reader = reader;
         }
 
-        private async Task<FunctionSnapshot[]> GetSnapshotsAsync()
+        private async Task<FunctionSnapshot[]> GetSnapshotsAsync(string hostName = null)
         {
-            var segment = await _reader.GetFunctionDefinitionsAsync(null);
+            var segment = await _reader.GetFunctionDefinitionsAsync(hostName, null);
             var definitions = segment.Results;
 
-            var current = _snapshots;
+            DateTime latest = GetLatestModifiedTime(definitions);
 
-            DateTime latest = GetLatestModifiedTime(definitions);            
+            var snapshots = Array.ConvertAll(definitions, definition => new FunctionSnapshot
+            {
+                Id = definition.FunctionId.ToString(),
+                FullName = definition.Name,
+                ShortName = definition.Name,
+                Parameters = new Dictionary<string, ParameterSnapshot>(),
+                HostVersion = definition.LastModified
+            });
+            _version = latest;
 
-            if ((_snapshots == null) || (latest > _version))
-            {          
-                _snapshots = Array.ConvertAll(definitions, definition => new FunctionSnapshot
-                {
-                    Id = definition.Name,
-                    FullName = definition.Name,
-                    ShortName = definition.Name,
-                    Parameters = new Dictionary<string, ParameterSnapshot>(),
-                    HostVersion = definition.LastModified
-                });
-                _version = latest;
-            }
-            return _snapshots;
+            return snapshots;
         }
 
         private static DateTime GetLatestModifiedTime(IFunctionDefinition[] definitions)
@@ -88,11 +82,11 @@ namespace Dashboard.Data
 
         FunctionStatistics IFunctionStatisticsReader.Lookup(string functionId)
         {
-            var theTask = Task.Run(() => LookupAsync(functionId));
+            var theTask = Task.Run(() => LookupAsync(FunctionId.DangerousFromString(functionId)));
             var retVal = theTask.GetAwaiter().GetResult();
             return retVal;
         }
-        private async Task<FunctionStatistics> LookupAsync(string functionId)
+        private async Task<FunctionStatistics> LookupAsync(FunctionId functionId)
         {
             var total = new FunctionStatistics
             {
@@ -133,16 +127,17 @@ namespace Dashboard.Data
             return _version;
         }
 
-        IResultSegment<FunctionIndexEntry> IFunctionIndexReader.Read(int maximumResults, string continuationToken)
+        IResultSegment<FunctionIndexEntry> IFunctionIndexReader.Read(string hostName, int maximumResults, string continuationToken)
         {
-            var theTask = Task.Run(() => Read1Async());
+            var theTask = Task.Run(() => Read1Async(hostName));
             var retVal = theTask.GetAwaiter().GetResult();
             return retVal;
         }
 
-        private async Task<IResultSegment<FunctionIndexEntry>> Read1Async()
+        private async Task<IResultSegment<FunctionIndexEntry>> Read1Async(string hostName)
         {
-            var snapshots = await GetSnapshotsAsync();
+            var snapshots = await GetSnapshotsAsync(hostName);
+
             var results = Array.ConvertAll(snapshots, x =>
                   FunctionIndexEntry.Create(
                         FunctionIndexEntry.CreateOtherMetadata(x), x.HostVersion));
@@ -162,13 +157,13 @@ namespace Dashboard.Data
             var endTime = FromContinuationToken(continuationToken, DateTime.MaxValue);
             var snapshots = await GetSnapshotsAsync();
 
-            string[] functionNames = Array.ConvertAll(snapshots, x => x.Id);
+            string[] functionIds = Array.ConvertAll(snapshots, x => x.Id);
 
             Task<Segment<IRecentFunctionEntry>>[] queryTasks = Array.ConvertAll(
-                functionNames,
-                functionName => _reader.GetRecentFunctionInstancesAsync(new RecentFunctionQuery
+                functionIds,
+                functionId => _reader.GetRecentFunctionInstancesAsync(new RecentFunctionQuery
                 {
-                    FunctionName = functionName,
+                    FunctionId = FunctionId.DangerousFromString(functionId),
                     MaximumResults = maximumResults,
                     Start = DateTime.MinValue, 
                     End = endTime                    
@@ -218,17 +213,17 @@ namespace Dashboard.Data
 
         IResultSegment<RecentInvocationEntry> IRecentInvocationIndexByFunctionReader.Read(string functionId, int maximumResults, string continuationToken)
         {
-            var theTask = Task.Run(() => Read3Async(functionId, maximumResults, continuationToken));
+            var theTask = Task.Run(() => Read3Async(FunctionId.DangerousFromString(functionId), maximumResults, continuationToken));
             var retVal = theTask.GetAwaiter().GetResult();
             return retVal;
         }
 
         private async Task<IResultSegment<RecentInvocationEntry>> Read3Async(
-            string functionId, int maximumResults, string continuationToken)
+            FunctionId functionId, int maximumResults, string continuationToken)
         {
             var queryParams = new RecentFunctionQuery
             {
-                FunctionName = functionId,
+                FunctionId = functionId,
                 MaximumResults = maximumResults,
                 Start = DateTime.MinValue,
                 End = DateTime.MaxValue

src/Microsoft.Azure.WebJobs.Logging/Entities/FunctionDefinitionEntity.cs
@@ -12,8 +12,8 @@ namespace Microsoft.Azure.WebJobs.Logging
     // 1 entity per function definition. 
     internal class FunctionDefinitionEntity : TableEntity, IFunctionDefinition, IEntityWithEpoch
     {
-        const string PartitionKeyFormat = TableScheme.FuncDefIndexPK;
-        const string RowKeyFormat = "{0}"; // functionName
+        const string PartitionKeyPrefix = TableScheme.FuncDefIndexPK;
+        const string RowKeyFormat = "{0}"; // FunctionId
 
         DateTime IFunctionDefinition.LastModified
         {
@@ -31,6 +31,17 @@ namespace Microsoft.Azure.WebJobs.Logging
             }
         }
 
+        // Host-aware name. Used in other APIs. 
+        // This value is already escaped. 
+        FunctionId IFunctionDefinition.FunctionId
+        {
+            get
+            {
+                return FunctionId.DangerousFromString(this.RowKey);
+            }
+        }
+        
+
         public DateTime GetEpoch()
         {
             return TimeBucket.CommonEpoch; // Definitions span all epocs 
@@ -40,13 +51,13 @@ namespace Microsoft.Azure.WebJobs.Logging
         // and functions must be case-preserving. 
         public string OriginalName { get; set; }
 
-        public static FunctionDefinitionEntity New(string functionName)
+        public static FunctionDefinitionEntity New(FunctionId functionId, string functionName)
         {
             return new FunctionDefinitionEntity
             {
-                PartitionKey = PartitionKeyFormat,
-                RowKey = string.Format(CultureInfo.InvariantCulture, RowKeyFormat, TableScheme.NormalizeFunctionName(functionName)),
-                OriginalName = functionName
+                PartitionKey = PartitionKeyPrefix,
+                RowKey = functionId.ToString(),
+                OriginalName = functionName                
             };
         }
 

src/Microsoft.Azure.WebJobs.Logging/FunctionId.cs
@@ -1,0 +1,108 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+using System;
+
+namespace Microsoft.Azure.WebJobs.Logging
+{
+    /// <summary>
+    /// Strongly typing for a function Id. 
+    /// FunctionIds include HostName and FunctionName
+    /// </summary>
+    public struct FunctionId
+    {
+        internal string Value { get; set; }
+
+        /// <summary>
+        /// Get string representation. This will be consist of valid Azure Table row key characters. 
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString()
+        {
+            return this.Value;
+        }
+
+        /// <summary>
+        /// Throw exception if this instance is bad. 
+        /// </summary>
+        public void Validate()
+        {
+            if (string.IsNullOrEmpty(this.Value))
+            {
+                throw new InvalidOperationException("FunctionId is not valid.");
+            }
+        }
+
+        /// <summary>
+        /// Build directly from a string. This skips escaping and should only be used by the reader for interop. 
+        /// This is the direct inverse of ToString().
+        /// </summary>
+        /// <param name="value">raw value from a previos ToString() call. </param>
+        /// <returns></returns>
+        public static FunctionId DangerousFromString(string value)
+        {
+            return new FunctionId { Value = value };
+        }
+
+        /// <summary>
+        /// Build a FunctionId from the host and function name. 
+        /// </summary>
+        /// <param name="hostName"></param>
+        /// <param name="functionName"></param>
+        /// <returns></returns>
+        public static FunctionId Build(string hostName, string functionName)
+        {
+            var value = string.Concat(
+                TableScheme.NormalizeFunctionName(hostName),
+                "-",
+                TableScheme.NormalizeFunctionName(functionName));
+            return DangerousFromString(value);
+        }
+
+        /// <summary>
+        /// Get a hashcode. 
+        /// </summary>
+        /// <returns></returns>
+        public override int GetHashCode()
+        {
+            return this.Value.GetHashCode();
+        }
+
+        /// <summary>
+        /// Equality. This is comparing normalized names. 
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        public override bool Equals(object obj)
+        {
+            if (obj is FunctionId)
+            {
+                var other = (FunctionId)obj;
+                return other.Value == this.Value;
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// Equality 
+        /// </summary>
+        /// <param name="left"></param>
+        /// <param name="right"></param>
+        /// <returns></returns>
+        public static bool operator ==(FunctionId left, FunctionId right)
+        {
+            return left.Value == right.Value;
+        }
+
+        /// <summary>
+        /// Inquality 
+        /// </summary>
+        /// <param name="left"></param>
+        /// <param name="right"></param>
+        /// <returns></returns>
+        public static bool operator !=(FunctionId left, FunctionId right)
+        {
+            return !(left == right);
+        }
+    }
+}

test/Microsoft.Azure.WebJobs.Logging.FunctionalTests/FunctionIdTests.cs
@@ -1,0 +1,46 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+using Xunit;
+
+namespace Microsoft.Azure.WebJobs.Logging.FunctionalTests
+{
+    public class FunctionIdTests
+    {
+        // Test normalization 
+        [Fact]
+        public void Operators()
+        {
+            var f1 = FunctionId.Build("h1", "FFF1");
+            var f2 = FunctionId.Build("h1", "fff1");
+
+            Assert.Equal("h1-fff1", f2.ToString());
+
+            // Names have been normalized.
+            Assert.True(f1.Equals(f2));
+            Assert.Equal(f1.GetHashCode(), f2.GetHashCode());
+            Assert.True(f1 == f2);
+            Assert.False(f1 != f2);
+        }
+
+        // Characters are escaped. 
+        [Fact]
+        public void Escapes()
+        {
+            var f = FunctionId.Build("h-1", "F-:1");
+
+            string str = f.ToString();
+            Assert.Equal("h:2D1-f:2D:3A1", str);
+        }
+
+        // Direct creation doesn't roundtrip. 
+        [Fact]
+        public void Direct()
+        {
+            string str = "ab-def%:x$";
+            var f = FunctionId.DangerousFromString(str);
+                        
+            Assert.Equal(str, f.ToString());
+        }
+    }
+}

alrod/azure-functions-servicebus-extension

src/Dashboard/FastTable/FastTableReader.cs
@@ -25,35 +25,29 @@ namespace Dashboard.Data
 
         private DateTime _version = DateTime.MinValue;
 
-        private FunctionSnapshot[] _snapshots = null; // Cache of function definitions 
-
         public FastTableReader(ILogReader reader)
         {
             _reader = reader;
         }
 
-        private async Task<FunctionSnapshot[]> GetSnapshotsAsync()
+        private async Task<FunctionSnapshot[]> GetSnapshotsAsync(string hostName = null)
         {
-            var segment = await _reader.GetFunctionDefinitionsAsync(null);
+            var segment = await _reader.GetFunctionDefinitionsAsync(hostName, null);
             var definitions = segment.Results;
 
-            var current = _snapshots;
+            DateTime latest = GetLatestModifiedTime(definitions);
 
-            DateTime latest = GetLatestModifiedTime(definitions);            
+            var snapshots = Array.ConvertAll(definitions, definition => new FunctionSnapshot
+            {
+                Id = definition.FunctionId.ToString(),
+                FullName = definition.Name,
+                ShortName = definition.Name,
+                Parameters = new Dictionary<string, ParameterSnapshot>(),
+                HostVersion = definition.LastModified
+            });
+            _version = latest;
 
-            if ((_snapshots == null) || (latest > _version))
-            {          
-                _snapshots = Array.ConvertAll(definitions, definition => new FunctionSnapshot
-                {
-                    Id = definition.Name,
-                    FullName = definition.Name,
-                    ShortName = definition.Name,
-                    Parameters = new Dictionary<string, ParameterSnapshot>(),
-                    HostVersion = definition.LastModified
-                });
-                _version = latest;
-            }
-            return _snapshots;
+            return snapshots;
         }
 
         private static DateTime GetLatestModifiedTime(IFunctionDefinition[] definitions)
@@ -88,11 +82,11 @@ namespace Dashboard.Data
 
         FunctionStatistics IFunctionStatisticsReader.Lookup(string functionId)
         {
-            var theTask = Task.Run(() => LookupAsync(functionId));
+            var theTask = Task.Run(() => LookupAsync(FunctionId.Parse(functionId)));
             var retVal = theTask.GetAwaiter().GetResult();
             return retVal;
         }
-        private async Task<FunctionStatistics> LookupAsync(string functionId)
+        private async Task<FunctionStatistics> LookupAsync(FunctionId functionId)
         {
             var total = new FunctionStatistics
             {
@@ -133,16 +127,17 @@ namespace Dashboard.Data
             return _version;
         }
 
-        IResultSegment<FunctionIndexEntry> IFunctionIndexReader.Read(int maximumResults, string continuationToken)
+        IResultSegment<FunctionIndexEntry> IFunctionIndexReader.Read(string hostName, int maximumResults, string continuationToken)
         {
-            var theTask = Task.Run(() => Read1Async());
+            var theTask = Task.Run(() => Read1Async(hostName));
             var retVal = theTask.GetAwaiter().GetResult();
             return retVal;
         }
 
-        private async Task<IResultSegment<FunctionIndexEntry>> Read1Async()
+        private async Task<IResultSegment<FunctionIndexEntry>> Read1Async(string hostName)
         {
-            var snapshots = await GetSnapshotsAsync();
+            var snapshots = await GetSnapshotsAsync(hostName);
+
             var results = Array.ConvertAll(snapshots, x =>
                   FunctionIndexEntry.Create(
                         FunctionIndexEntry.CreateOtherMetadata(x), x.HostVersion));
@@ -162,13 +157,13 @@ namespace Dashboard.Data
             var endTime = FromContinuationToken(continuationToken, DateTime.MaxValue);
             var snapshots = await GetSnapshotsAsync();
 
-            string[] functionNames = Array.ConvertAll(snapshots, x => x.Id);
+            string[] functionIds = Array.ConvertAll(snapshots, x => x.Id);
 
             Task<Segment<IRecentFunctionEntry>>[] queryTasks = Array.ConvertAll(
-                functionNames,
-                functionName => _reader.GetRecentFunctionInstancesAsync(new RecentFunctionQuery
+                functionIds,
+                functionId => _reader.GetRecentFunctionInstancesAsync(new RecentFunctionQuery
                 {
-                    FunctionName = functionName,
+                    FunctionId = FunctionId.Parse(functionId),
                     MaximumResults = maximumResults,
                     Start = DateTime.MinValue, 
                     End = endTime                    
@@ -218,17 +213,17 @@ namespace Dashboard.Data
 
         IResultSegment<RecentInvocationEntry> IRecentInvocationIndexByFunctionReader.Read(string functionId, int maximumResults, string continuationToken)
         {
-            var theTask = Task.Run(() => Read3Async(functionId, maximumResults, continuationToken));
+            var theTask = Task.Run(() => Read3Async(FunctionId.Parse(functionId), maximumResults, continuationToken));
             var retVal = theTask.GetAwaiter().GetResult();
             return retVal;
         }
 
         private async Task<IResultSegment<RecentInvocationEntry>> Read3Async(
-            string functionId, int maximumResults, string continuationToken)
+            FunctionId functionId, int maximumResults, string continuationToken)
         {
             var queryParams = new RecentFunctionQuery
             {
-                FunctionName = functionId,
+                FunctionId = functionId,
                 MaximumResults = maximumResults,
                 Start = DateTime.MinValue,
                 End = DateTime.MaxValue

src/Microsoft.Azure.WebJobs.Logging/Entities/FunctionDefinitionEntity.cs
@@ -12,8 +12,8 @@ namespace Microsoft.Azure.WebJobs.Logging
     // 1 entity per function definition. 
     internal class FunctionDefinitionEntity : TableEntity, IFunctionDefinition, IEntityWithEpoch
     {
-        const string PartitionKeyFormat = TableScheme.FuncDefIndexPK;
-        const string RowKeyFormat = "{0}"; // functionName
+        const string PartitionKeyPrefix = TableScheme.FuncDefIndexPK;
+        const string RowKeyFormat = "{0}"; // FunctionId
 
         DateTime IFunctionDefinition.LastModified
         {
@@ -31,6 +31,17 @@ namespace Microsoft.Azure.WebJobs.Logging
             }
         }
 
+        // Host-aware name. Used in other APIs. 
+        // This value is already escaped. 
+        FunctionId IFunctionDefinition.FunctionId
+        {
+            get
+            {
+                return FunctionId.Parse(this.RowKey);
+            }
+        }
+        
+
         public DateTime GetEpoch()
         {
             return TimeBucket.CommonEpoch; // Definitions span all epocs 
@@ -40,13 +51,13 @@ namespace Microsoft.Azure.WebJobs.Logging
         // and functions must be case-preserving. 
         public string OriginalName { get; set; }
 
-        public static FunctionDefinitionEntity New(string functionName)
+        public static FunctionDefinitionEntity New(FunctionId functionId, string functionName)
         {
             return new FunctionDefinitionEntity
             {
-                PartitionKey = PartitionKeyFormat,
-                RowKey = string.Format(CultureInfo.InvariantCulture, RowKeyFormat, TableScheme.NormalizeFunctionName(functionName)),
-                OriginalName = functionName
+                PartitionKey = PartitionKeyPrefix,
+                RowKey = functionId.ToString(),
+                OriginalName = functionName                
             };
         }
 

src/Microsoft.Azure.WebJobs.Logging/FunctionId.cs
@@ -1,0 +1,108 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+using System;
+
+namespace Microsoft.Azure.WebJobs.Logging
+{
+    /// <summary>
+    /// Strongly typing for a function Id. 
+    /// FunctionIds include HostName and FunctionName
+    /// </summary>
+    public struct FunctionId
+    {
+        internal string Value { get; set; }
+
+        /// <summary>
+        /// Get string representation. This will be consist of valid Azure Table row key characters. 
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString()
+        {
+            return this.Value;
+        }
+
+        /// <summary>
+        /// Throw exception if this instance is bad. 
+        /// </summary>
+        public void Validate()
+        {
+            if (string.IsNullOrEmpty(this.Value))
+            {
+                throw new InvalidOperationException("FunctionId is not valid.");
+            }
+        }
+
+        /// <summary>
+        /// Build directly from a string. This skips escaping and should only be used by the reader for interop. 
+        /// This is the direct inverse of ToString().
+        /// </summary>
+        /// <param name="value">raw value from a previous ToString() call. </param>
+        /// <returns></returns>
+        public static FunctionId Parse(string value)
+        {
+            return new FunctionId { Value = value };
+        }
+
+        /// <summary>
+        /// Build a FunctionId from the host and function name. 
+        /// </summary>
+        /// <param name="hostName"></param>
+        /// <param name="functionName"></param>
+        /// <returns></returns>
+        public static FunctionId Build(string hostName, string functionName)
+        {
+            var value = string.Concat(
+                TableScheme.NormalizeFunctionName(hostName),
+                "-",
+                TableScheme.NormalizeFunctionName(functionName));
+            return Parse(value);
+        }
+
+        /// <summary>
+        /// Get a hashcode. 
+        /// </summary>
+        /// <returns></returns>
+        public override int GetHashCode()
+        {
+            return this.Value.GetHashCode();
+        }
+
+        /// <summary>
+        /// Equality. This is comparing normalized names. 
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        public override bool Equals(object obj)
+        {
+            if (obj is FunctionId)
+            {
+                var other = (FunctionId)obj;
+                return other.Value == this.Value;
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// Equality 
+        /// </summary>
+        /// <param name="left"></param>
+        /// <param name="right"></param>
+        /// <returns></returns>
+        public static bool operator ==(FunctionId left, FunctionId right)
+        {
+            return left.Value == right.Value;
+        }
+
+        /// <summary>
+        /// Inquality 
+        /// </summary>
+        /// <param name="left"></param>
+        /// <param name="right"></param>
+        /// <returns></returns>
+        public static bool operator !=(FunctionId left, FunctionId right)
+        {
+            return !(left == right);
+        }
+    }
+}

test/Microsoft.Azure.WebJobs.Logging.FunctionalTests/FunctionIdTests.cs
@@ -1,0 +1,46 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+using Xunit;
+
+namespace Microsoft.Azure.WebJobs.Logging.FunctionalTests
+{
+    public class FunctionIdTests
+    {
+        // Test normalization 
+        [Fact]
+        public void Operators()
+        {
+            var f1 = FunctionId.Build("h1", "FFF1");
+            var f2 = FunctionId.Build("H1", "fff1"); // different casing. 
+
+            Assert.Equal("h1-fff1", f2.ToString());
+
+            // Names have been normalized.
+            Assert.True(f1.Equals(f2));
+            Assert.Equal(f1.GetHashCode(), f2.GetHashCode());
+            Assert.True(f1 == f2);
+            Assert.False(f1 != f2);
+        }
+
+        // Characters are escaped. 
+        [Fact]
+        public void Escapes()
+        {
+            var f = FunctionId.Build("h-1", "F-:1");
+
+            string str = f.ToString();
+            Assert.Equal("h:2D1-f:2D:3A1", str);
+        }
+
+        // Direct creation doesn't roundtrip. 
+        [Fact]
+        public void Direct()
+        {
+            string str = "ab-def%:x$";
+            var f = FunctionId.Parse(str);
+                        
+            Assert.Equal(str, f.ToString());
+        }
+    }
+}
NiklasArbin/azure-webjobs-sdk

Adding Account properties onto base Azure Storage binding attributes

src/Microsoft.Azure.WebJobs.Host/Executors/StorageAccountProviderExtensions.cs
@@ -34,28 +34,11 @@ namespace Microsoft.Azure.WebJobs.Host.Executors
             return account;
         }
 
-        public static async Task<IStorageAccount> GetAccountAsync(this IStorageAccountProvider provider, string connectionStringName, CancellationToken cancellationToken)
+        public static async Task<IStorageAccount> GetStorageAccountAsync(this IStorageAccountProvider provider, string connectionStringName, CancellationToken cancellationToken, INameResolver nameResolver = null)
         {
             if (provider == null)
             {
                 throw new ArgumentNullException("provider");
-            }
-
-            IStorageAccount account = await provider.TryGetAccountAsync(connectionStringName, cancellationToken);
-            ValidateStorageAccount(account, connectionStringName);
-            return account;
-        }
-        public static async Task<IStorageAccount> GetStorageAccountAsync(this IStorageAccountProvider provider, ParameterInfo parameter, CancellationToken cancellationToken, INameResolver nameResolver = null)
-        {
-            if (provider == null)
-            {
-                throw new ArgumentNullException("provider");
-            }
-
-            string connectionStringName = GetAccountOverrideOrNull(parameter);
-            if (string.IsNullOrEmpty(connectionStringName))
-            {
-                connectionStringName = ConnectionStringNames.Storage;
             }
 
             if (nameResolver != null)
@@ -72,6 +55,27 @@ namespace Microsoft.Azure.WebJobs.Host.Executors
             return account;
         }
 
+        public static async Task<IStorageAccount> GetStorageAccountAsync(this IStorageAccountProvider provider, StorageAttribute storageAttribute, CancellationToken cancellationToken, INameResolver nameResolver = null)
+        {
+            return await provider.GetStorageAccountAsync(storageAttribute.Account ?? ConnectionStringNames.Storage, cancellationToken, nameResolver);
+        }
+
+        public static async Task<IStorageAccount> GetStorageAccountAsync(this IStorageAccountProvider provider, ParameterInfo parameter, CancellationToken cancellationToken, INameResolver nameResolver = null)
+        {
+            if (provider == null)
+            {
+                throw new ArgumentNullException("provider");
+            }
+
+            string connectionStringName = GetAccountOverrideOrNull(parameter);
+            if (string.IsNullOrEmpty(connectionStringName))
+            {
+                connectionStringName = ConnectionStringNames.Storage;
+            }
+
+            return await provider.GetStorageAccountAsync(connectionStringName, cancellationToken, nameResolver);
+        }
+
         private static void ValidateStorageAccount(IStorageAccount account, string connectionStringName)
         {
             if (account == null)
@@ -82,16 +86,27 @@ namespace Microsoft.Azure.WebJobs.Host.Executors
         }
 
         /// <summary>
-        /// Walk from the parameter up to the containing type, looking for a
-        /// <see cref="StorageAccountAttribute"/>. If found, return the account.
+        /// Determine whether the specified parameter declares an explicit storage
+        /// account to use, either by specifying a value for <see cref="StorageAttribute.Account"/> or
+        /// if <see cref="StorageAccountAttribute"/> has been applied.
         /// </summary>
         internal static string GetAccountOverrideOrNull(ParameterInfo parameter)
         {
-            StorageAccountAttribute attribute = TypeUtility.GetHierarchicalAttributeOrNull<StorageAccountAttribute>(parameter);
-            if (attribute != null)
+            // if this is a Storage attribute (e.g. Queues/Blobs/Tables) and
+            // it specifies an account, return it
+            var storageAttribute = parameter.GetCustomAttribute<StorageAttribute>();
+            if (storageAttribute != null && !string.IsNullOrEmpty(storageAttribute.Account))
             {
-                return attribute.Account;
+                return storageAttribute.Account;
             }
+
+            // walk up from the parameter looking for any StorageAccountAttribute overrides
+            var storageAccountAttribute = TypeUtility.GetHierarchicalAttributeOrNull<StorageAccountAttribute>(parameter);
+            if (storageAccountAttribute != null)
+            {
+                return storageAccountAttribute.Account;
+            }
+
             return null;
         }
     }

test/Microsoft.Azure.WebJobs.Host.EndToEndTests/MultipleStorageAccountsEndToEndTests.cs
@@ -166,13 +166,13 @@ namespace Microsoft.Azure.WebJobs.Host.EndToEndTests
 
         public static void BlobToBlob_DifferentAccounts_PrimaryToSecondary(
             [BlobTrigger(Input + "/{name}")] string input,
-            [Blob(Output + "/{name}"), StorageAccount(Secondary)] out string output)
+            [Blob(Output + "/{name}", Account = Secondary)] out string output)
         {
             output = input;
         }
 
         public static void BlobToBlob_DifferentAccounts_SecondaryToPrimary(
-            [BlobTrigger(Input + "/{name}"), StorageAccount(Secondary)] string input,
+            [BlobTrigger(Input + "/{name}", Account = Secondary)] string input,
             [Blob(Output + "/{name}")] out string output)
         {
             output = input;
@@ -181,7 +181,7 @@ namespace Microsoft.Azure.WebJobs.Host.EndToEndTests
 
         public static void QueueToQueue_DifferentAccounts_PrimaryToSecondary(
             [QueueTrigger(Input)] string input,
-            [Queue(Output), StorageAccount(Secondary)] out string output)
+            [Queue(Output, Account = Secondary)] out string output)
         {
             output = input;
         }
@@ -189,7 +189,7 @@ namespace Microsoft.Azure.WebJobs.Host.EndToEndTests
         [NoAutomaticTrigger]
         public static void QueueToBlob_DifferentAccounts_PrimaryToSecondary_NameResolver(
             [QueueTrigger("test")] Message input,
-            [Blob(Output + "/{Name}"), StorageAccount("%test_account%")] out string output)
+            [Blob(Output + "/{Name}", Account = "%test_account%")] out string output)
         {
             output = input.Value;
         }
@@ -197,13 +197,13 @@ namespace Microsoft.Azure.WebJobs.Host.EndToEndTests
         [NoAutomaticTrigger]
         public static void QueueToBlob_DifferentAccounts_PrimaryToSecondary_FullSettingName(
             [QueueTrigger("test")] Message input,
-            [Blob(Output + "/{Name}"), StorageAccount("AzureWebJobsSecondaryStorage")] out string output)
+            [Blob(Output + "/{Name}", Account = "AzureWebJobsSecondaryStorage")] out string output)
         {
             output = input.Value;
         }
 
         public static void QueueToQueue_DifferentAccounts_SecondaryToPrimary(
-            [QueueTrigger(Input), StorageAccount(Secondary)] string input,
+            [QueueTrigger(Input, Account = Secondary)] string input,
             [Queue(Output)] out string output)
         {
             output = input;
@@ -212,7 +212,7 @@ namespace Microsoft.Azure.WebJobs.Host.EndToEndTests
         [NoAutomaticTrigger]
         public static void Table_PrimaryAndSecondary(
             [Table(OutputTableName)] CloudTable primaryOutput,
-            [Table(OutputTableName), StorageAccount(Secondary)] CloudTable secondaryOutput)
+            [Table(OutputTableName, Account = Secondary)] CloudTable secondaryOutput)
         {
             TestTableEntity entity = new TestTableEntity
             {

alrod/azure-functions-servicebus-extension

src/Microsoft.Azure.WebJobs.Host/Executors/StorageAccountProviderExtensions.cs
@@ -34,29 +34,14 @@ namespace Microsoft.Azure.WebJobs.Host.Executors
             return account;
         }
 
-        public static async Task<IStorageAccount> GetAccountAsync(this IStorageAccountProvider provider, string connectionStringName, CancellationToken cancellationToken)
+        public static async Task<IStorageAccount> GetStorageAccountAsync(this IStorageAccountProvider provider, string connectionStringName, CancellationToken cancellationToken, INameResolver nameResolver = null)
         {
             if (provider == null)
             {
                 throw new ArgumentNullException("provider");
             }
 
-            IStorageAccount account = await provider.TryGetAccountAsync(connectionStringName, cancellationToken);
-            ValidateStorageAccount(account, connectionStringName);
-            return account;
-        }
-        public static async Task<IStorageAccount> GetStorageAccountAsync(this IStorageAccountProvider provider, ParameterInfo parameter, CancellationToken cancellationToken, INameResolver nameResolver = null)
-        {
-            if (provider == null)
-            {
-                throw new ArgumentNullException("provider");
-            }
-
-            string connectionStringName = GetAccountOverrideOrNull(parameter);
-            if (string.IsNullOrEmpty(connectionStringName))
-            {
-                connectionStringName = ConnectionStringNames.Storage;
-            }
+            connectionStringName = connectionStringName ?? ConnectionStringNames.Storage;
 
             if (nameResolver != null)
             {
@@ -72,6 +57,11 @@ namespace Microsoft.Azure.WebJobs.Host.Executors
             return account;
         }
 
+        public static async Task<IStorageAccount> GetStorageAccountAsync(this IStorageAccountProvider provider, IConnectionProvider connectionProvider, CancellationToken cancellationToken, INameResolver nameResolver = null)
+        {
+            return await provider.GetStorageAccountAsync(connectionProvider.Connection, cancellationToken, nameResolver);
+        }
+
         private static void ValidateStorageAccount(IStorageAccount account, string connectionStringName)
         {
             if (account == null)
@@ -82,16 +72,27 @@ namespace Microsoft.Azure.WebJobs.Host.Executors
         }
 
         /// <summary>
-        /// Walk from the parameter up to the containing type, looking for a
-        /// <see cref="StorageAccountAttribute"/>. If found, return the account.
+        /// Determine whether the specified parameter declares an explicit storage
+        /// account to use, either by specifying a value for <see cref="IConnectionProvider.Connection"/> or
+        /// if <see cref="StorageAccountAttribute"/> has been applied up the hierarchy.
         /// </summary>
         internal static string GetAccountOverrideOrNull(ParameterInfo parameter)
         {
-            StorageAccountAttribute attribute = TypeUtility.GetHierarchicalAttributeOrNull<StorageAccountAttribute>(parameter);
-            if (attribute != null)
+            // if this is a Storage attribute (e.g. Queues/Blobs/Tables) and
+            // it specifies an account, return it
+            var storageAttribute = parameter.GetCustomAttribute<StorageAccountAttribute>();
+            if (storageAttribute != null && !string.IsNullOrEmpty(storageAttribute.Account))
             {
-                return attribute.Account;
+                return storageAttribute.Account;
             }
+
+            // walk up from the parameter looking for any StorageAccountAttribute overrides
+            var storageAccountAttribute = TypeUtility.GetHierarchicalAttributeOrNull<StorageAccountAttribute>(parameter);
+            if (storageAccountAttribute != null)
+            {
+                return storageAccountAttribute.Account;
+            }
+
             return null;
         }
     }

test/Microsoft.Azure.WebJobs.Host.EndToEndTests/MultipleStorageAccountsEndToEndTests.cs
@@ -166,13 +166,13 @@ namespace Microsoft.Azure.WebJobs.Host.EndToEndTests
 
         public static void BlobToBlob_DifferentAccounts_PrimaryToSecondary(
             [BlobTrigger(Input + "/{name}")] string input,
-            [Blob(Output + "/{name}"), StorageAccount(Secondary)] out string output)
+            [Blob(Output + "/{name}", Connection = Secondary)] out string output)
         {
             output = input;
         }
 
         public static void BlobToBlob_DifferentAccounts_SecondaryToPrimary(
-            [BlobTrigger(Input + "/{name}"), StorageAccount(Secondary)] string input,
+            [BlobTrigger(Input + "/{name}", Connection = Secondary)] string input,
             [Blob(Output + "/{name}")] out string output)
         {
             output = input;
@@ -181,7 +181,7 @@ namespace Microsoft.Azure.WebJobs.Host.EndToEndTests
 
         public static void QueueToQueue_DifferentAccounts_PrimaryToSecondary(
             [QueueTrigger(Input)] string input,
-            [Queue(Output), StorageAccount(Secondary)] out string output)
+            [Queue(Output, Connection = Secondary)] out string output)
         {
             output = input;
         }
@@ -189,7 +189,7 @@ namespace Microsoft.Azure.WebJobs.Host.EndToEndTests
         [NoAutomaticTrigger]
         public static void QueueToBlob_DifferentAccounts_PrimaryToSecondary_NameResolver(
             [QueueTrigger("test")] Message input,
-            [Blob(Output + "/{Name}"), StorageAccount("%test_account%")] out string output)
+            [Blob(Output + "/{Name}", Connection = "%test_account%")] out string output)
         {
             output = input.Value;
         }
@@ -197,13 +197,13 @@ namespace Microsoft.Azure.WebJobs.Host.EndToEndTests
         [NoAutomaticTrigger]
         public static void QueueToBlob_DifferentAccounts_PrimaryToSecondary_FullSettingName(
             [QueueTrigger("test")] Message input,
-            [Blob(Output + "/{Name}"), StorageAccount("AzureWebJobsSecondaryStorage")] out string output)
+            [Blob(Output + "/{Name}", Connection = "AzureWebJobsSecondaryStorage")] out string output)
         {
             output = input.Value;
         }
 
         public static void QueueToQueue_DifferentAccounts_SecondaryToPrimary(
-            [QueueTrigger(Input), StorageAccount(Secondary)] string input,
+            [QueueTrigger(Input, Connection = Secondary)] string input,
             [Queue(Output)] out string output)
         {
             output = input;
@@ -212,7 +212,7 @@ namespace Microsoft.Azure.WebJobs.Host.EndToEndTests
         [NoAutomaticTrigger]
         public static void Table_PrimaryAndSecondary(
             [Table(OutputTableName)] CloudTable primaryOutput,
-            [Table(OutputTableName), StorageAccount(Secondary)] CloudTable secondaryOutput)
+            [Table(OutputTableName, Connection = Secondary)] CloudTable secondaryOutput)
         {
             TestTableEntity entity = new TestTableEntity
             {